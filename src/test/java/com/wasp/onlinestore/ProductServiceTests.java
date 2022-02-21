e4fa59e8053fe5b4680a521321b3126f8056303a
wasp <wasp@rocke.tech>
2022-02-21 13:01:05 +0200

    add getById() test

--- src/test/java/com/wasp/onlinestore/ProductServiceTests.java
+++ src/test/java/com/wasp/onlinestore/ProductServiceTests.java
@@ -6,21 +6,25 @@ import com.wasp.onlinestore.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ProductServiceTests {
+    private Date date;
     private Product chair;
     private ProductService productService;
     private JdbcProductDao mockedDao;
 
     @BeforeEach
     void init() {
-        chair = Product.builder().id(1).name("chair").price(10).build();
+        date = new Date();
+        chair = Product.builder().id(1).name("chair").price(10).creationDate(date).build();
         mockedDao = mock(JdbcProductDao.class);
         productService = new ProductService(mockedDao);
     }
@@ -37,6 +41,14 @@ class ProductServiceTests {
         }
 
         assertEquals(0, actualProducts.size());
+    }
+
+    @Test
+    void getById() {
+        when(mockedDao.findById(1)).thenReturn(chair);
+
+        assertEquals("chair", productService.getById(1).getName());
+        assertEquals(10, productService.getById(1).getPrice());
     }
 
     @Test
