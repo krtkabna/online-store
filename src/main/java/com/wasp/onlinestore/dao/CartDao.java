a282267210be1d39d551d0a55ea9a9c94e09736b
wasp <wasp@rocke.tech>
2022-03-16 13:23:42 +0100

    remove cart dao

--- src/main/java/com/wasp/onlinestore/dao/CartDao.java
+++
@@ -1,9 +0,0 @@
-package com.wasp.onlinestore.dao;
-
-import com.wasp.onlinestore.entity.Product;
-
-public interface CartDao {
-    Iterable<Product> findAll(String token);
-    boolean add(int productId);
-    boolean remove(int productId);
-}
