999da552f5384c341882e5f873872b5d964cfdba
wasp <wasp@rocke.tech>
2022-03-23 12:50:21 +0100

    merge master, remove userService and securityService(for now)

--- src/main/java/com/wasp/onlinestore/main/ServiceLocator.java
+++ src/main/java/com/wasp/onlinestore/main/ServiceLocator.java
@@ -2,14 +2,8 @@ package com.wasp.onlinestore.main;
 
 import com.wasp.onlinestore.config.PropertyReader;
 import com.wasp.onlinestore.dao.ProductDao;
-import com.wasp.onlinestore.dao.UserDao;
 import com.wasp.onlinestore.dao.jdbc.JdbcProductDao;
-import com.wasp.onlinestore.dao.jdbc.JdbcUserDao;
-import com.wasp.onlinestore.service.CartService;
 import com.wasp.onlinestore.service.ProductService;
-import com.wasp.onlinestore.service.SessionService;
-import com.wasp.onlinestore.service.UserService;
-import com.wasp.onlinestore.service.security.SecurityService;
 import com.wasp.onlinestore.web.util.PageGenerator;
 import org.postgresql.ds.PGSimpleDataSource;
 import java.util.HashMap;
@@ -31,18 +25,13 @@ public class ServiceLocator {
 
         //config dao
         ProductDao productDao = new JdbcProductDao(pgSimpleDataSource);
-        UserDao userDao = new JdbcUserDao(pgSimpleDataSource);
 
         //config service
         PageGenerator pageGenerator = new PageGenerator();
         ProductService productService = new ProductService(productDao);
-        UserService userService = new UserService(userDao);
-        SecurityService securityService = new SecurityService(userService);
 
         CONTEXT.put(PageGenerator.class, pageGenerator);
         CONTEXT.put(ProductService.class, productService);
-        CONTEXT.put(UserService.class, userService);
-        CONTEXT.put(SecurityService.class, securityService);
     }
 
     public static <T> T get(Class<T> clazz) {
